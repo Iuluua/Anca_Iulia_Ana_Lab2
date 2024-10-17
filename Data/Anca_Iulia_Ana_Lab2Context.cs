495af827b9c23c836c6ac9fa11e51877a4150283
Iulia Anca <iulia.anca@evozon.com>
2024-10-17 17:16:12 +0300

    Add project files.

---
+++ Data/Anca_Iulia_Ana_Lab2Context.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Anca_Iulia_Ana_Lab2.Models;
+
+namespace Anca_Iulia_Ana_Lab2.Data
+{
+    public class Anca_Iulia_Ana_Lab2Context : DbContext
+    {
+        public Anca_Iulia_Ana_Lab2Context (DbContextOptions<Anca_Iulia_Ana_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Anca_Iulia_Ana_Lab2.Models.Book> Book { get; set; } = default!;
+        public DbSet<Anca_Iulia_Ana_Lab2.Models.Publisher> Publisher { get; set; } = default!;
+    }
+}
---
+++ Models/Publisher.cs
@@ -0,0 +1,11 @@
+﻿namespace Anca_Iulia_Ana_Lab2.Models
+{
+    public class Publisher
+    {
+        public int ID { get; set; }
+
+        public string PublisherName { get; set; }
+
+        public ICollection<Book>? Books { get; set; }  //navigation property
+    }
+}
