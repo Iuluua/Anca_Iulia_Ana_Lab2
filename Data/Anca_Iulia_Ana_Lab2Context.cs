0488c838958a0208a9d48112b43e9d38284633c6
Iulia Anca <iulia.anca@evozon.com>
2024-10-17 19:20:33 +0300

    Add page for Authors

--- Data/Anca_Iulia_Ana_Lab2Context.cs
+++ Data/Anca_Iulia_Ana_Lab2Context.cs
@@ -16,5 +16,6 @@ namespace Anca_Iulia_Ana_Lab2.Data
 
         public DbSet<Anca_Iulia_Ana_Lab2.Models.Book> Book { get; set; } = default!;
         public DbSet<Anca_Iulia_Ana_Lab2.Models.Publisher> Publisher { get; set; } = default!;
+        public DbSet<Anca_Iulia_Ana_Lab2.Models.Author> Author { get; set; } = default!;
     }
 }
