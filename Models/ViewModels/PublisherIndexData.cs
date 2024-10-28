a5ac3a8947a20df6b1ef6f0f5d28b283de52311e
Iulia Anca <iulia.anca@evozon.com>
2024-10-28 13:35:44 +0200

    Modificari lab4 pana la ex5

---
+++ Models/ViewModels/PublisherIndexData.cs
@@ -0,0 +1,10 @@
+﻿using Anca_Iulia_Ana_Lab2.Models;
+
+namespace Anca_Iulia_Ana_Lab2.Models.ViewModels
+{
+    public class PublisherIndexData
+    {
+        public IEnumerable<Publisher> Publishers { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+    }
+}
