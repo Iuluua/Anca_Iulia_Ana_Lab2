63a31c159ac6a1aba4d563c9764671db1a72ea88
Iulia Anca <iulia.anca@evozon.com>
2024-10-29 12:40:24 +0200

    Rezolvare sarcina laborator

---
+++ Models/ViewModels/CategoryIndexData.cs
@@ -0,0 +1,10 @@
+﻿using Anca_Iulia_Ana_Lab2.Models;
+
+namespace Anca_Iulia_Ana_Lab2.Models.ViewModels
+{
+    public class CategoryIndexData
+    {
+        public IEnumerable<Category> Categories { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+    }
+}
--- Pages/Categories/Index.cshtml.cs
+++ Pages/Categories/Index.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Anca_Iulia_Ana_Lab2.Data;
 using Anca_Iulia_Ana_Lab2.Models;
+using Anca_Iulia_Ana_Lab2.Models.ViewModels;
 
 namespace Anca_Iulia_Ana_Lab2.Pages.Categories
 {
@@ -21,9 +22,32 @@ namespace Anca_Iulia_Ana_Lab2.Pages.Categories
 
         public IList<Category> Category { get;set; } = default!;
 
-        public async Task OnGetAsync()
+        public CategoryIndexData CategoryData { get; set; }
+        public int CategoryID { get; set; }
+        public int BookID { get; set; }
+
+        public async Task OnGetAsync(int? id, int? bookID)
         {
-            Category = await _context.Category.ToListAsync();
+            CategoryData = new CategoryIndexData();
+            CategoryData.Categories = await _context.Category
+                .Include(i => i.BookCategories)
+                    .ThenInclude(i => i.Book)
+                    .ThenInclude(c => c.Author)
+                .OrderBy(i => i.CategoryName)
+                .ToListAsync();
+
+            if (id != null)
+            {
+                CategoryID = id.Value;
+                Category category = CategoryData.Categories
+                    .Where(i => i.ID == id.Value).Single();
+                CategoryData.Books = category.BookCategories.Select(bc => bc.Book);
+            }
         }
-    }
+
+    //public async Task OnGetAsync()
+    //{
+    //    Category = await _context.Category.ToListAsync();
+    //}
+}
 }
